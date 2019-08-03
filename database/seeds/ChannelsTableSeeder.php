537edca0e6dc7ae9edfee66369284671a08b1b43
Abdul Hakim <hakimbdul8@gmail.com>
2019-08-03 17:13:26 +0700

    Initial commit

---
+++ database/seeds/ChannelsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Channel;
+use Illuminate\Database\Seeder;
+
+class ChannelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Channel::create([
+           'name' => 'Laravel 5.3',
+           'slug' => str_slug('Laravel 5.3'),
+        ]);
+
+        Channel::create([
+            'name' => 'Vue Js 3',
+            'slug' => str_slug('Vue Js 3'),
+        ]);
+
+        Channel::create([
+            'name' => 'Angular 7',
+            'slug' => str_slug('Angular 7'),
+        ]);
+
+        Channel::create([
+            'name' => 'Node js',
+            'slug' => str_slug('Node js'),
+        ]);
+    }
+}
